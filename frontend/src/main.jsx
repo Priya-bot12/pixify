44bcef2955d3d04b3f10c882316898f9278ccb0b
Priya-bot12 <119518888+Priya-bot12@users.noreply.github.com>
2025-05-13 21:04:13 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 
 import { createRoot } from 'react-dom/client'
 import {  BrowserRouter} from 'react-router-dom';
-import AppContextProvider from './context/appContext.jsx';
+import AppContextProvider from './context/AppContext.jsx';
 import './index.css'
 import App from './App.jsx'
 
