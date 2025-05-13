9999ca0ad0ad4afd8efee90eab933fbca34ff16f
Priya-bot12 <119518888+Priya-bot12@users.noreply.github.com>
2025-05-13 21:02:48 +0530

    Update GenerateBtn.jsx

--- frontend/src/components/GenerateBtn.jsx
+++ frontend/src/components/GenerateBtn.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { motion } from "motion/react"
-import { AppContext } from '../context/appContext'
+import { AppContext } from '../context/AppContext'
 import { useNavigate } from 'react-router-dom'
 
 const GenerateBtn = () => {
