5b44784121e7a2f5c8fca2029b609943cd26e4b8
Priya-bot12 <119518888+Priya-bot12@users.noreply.github.com>
2025-05-14 12:46:27 +0530

    Update BuyCredit.jsx

--- frontend/src/pages/BuyCredit.jsx
+++ frontend/src/pages/BuyCredit.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { assets, plans } from '../assets/assets'
-import { AppContext } from '../context/appContext'
+import { AppContext } from '../context/AppContext'
 import { motion } from "motion/react"
 import {useNavigate} from 'react-router-dom'
 import { toast } from 'react-toastify'
